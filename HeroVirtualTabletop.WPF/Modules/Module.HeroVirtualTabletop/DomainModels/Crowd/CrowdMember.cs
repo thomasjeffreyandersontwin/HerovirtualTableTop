5a1d357e939e79e73032dbbc73bcf896d6f5c515
istiaq <istiaq.ahmed.023@gmail.com>
2016-04-30 22:40:44 +1000

    Add CrowdMember Class

---
+++ HeroVirtualTabletop.WPF/Modules/Module.HeroVirtualTabletop/DomainModels/Crowd/CrowdMember.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module.HeroVirtualTabletop.DomainModels
+{
+    public class CrowdMember : Character, ICrowdMember
+    {
+        public string Name
+        {
+            get
+            {
+                return base.Name; // Name should be the Character's name
+            }
+            set
+            {
+                base.Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private ICrowdMember parent;
+        public ICrowdMember Parent
+        {
+            get
+            {
+                return parent;
+            }
+            set
+            {
+                parent = value;
+                OnPropertyChanged("Parent");
+            }
+        }
+
+        private ObservableCollection<ICrowdMember> crowdMemberCollection;
+        public ObservableCollection<ICrowdMember> CrowdMemberCollection
+        {
+            get
+            {
+                return crowdMemberCollection;
+            }
+            set
+            {
+                crowdMemberCollection = value;
+                OnPropertyChanged("CrowdMemberCollection");
+            }
+        }
+    }
+}
