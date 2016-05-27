de0956587a79bf0d10cea08580cca6c0a227ad9b
istiaq <istiaq.ahmed.023@gmail.com>
2016-05-27 22:48:14 +1000

    Click on character runs app action

--- HeroVirtualTabletop.WPF/Modules/Module.HeroVirtualTabletop/Rosters/RosterExplorerView.xaml.cs
+++ HeroVirtualTabletop.WPF/Modules/Module.HeroVirtualTabletop/Rosters/RosterExplorerView.xaml.cs
@@ -22,17 +22,28 @@ namespace Module.HeroVirtualTabletop.Roster
     public partial class RosterExplorerView : UserControl
     {
         private RosterExplorerViewModel viewModel;
+
+        private bool isSingleClick = false;
+        private bool isDoubleClick = false;
+        private bool isTripleClick = false;
+        private bool isQuadrupleClick = false;
+        private int milliseconds = 0;
+        private int maxClickTime = System.Windows.Forms.SystemInformation.DoubleClickTime * 3;
+        private System.Windows.Forms.Timer clickTimer = new System.Windows.Forms.Timer();
         public RosterExplorerView(RosterExplorerViewModel viewModel)
         {
             InitializeComponent();
 
             this.viewModel = viewModel;
             this.DataContext = this.viewModel;
-        }
 
+            clickTimer.Interval = 50;
+            clickTimer.Tick +=
+                new EventHandler(clickTimer_Tick);
+        }
         private void TextBlock_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed && Keyboard.Modifiers == ModifierKeys.Control)
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.LeftButton == MouseButtonState.Pressed)
             {
                 var ItemsPres = ((sender as TextBlock).Parent as Expander).Content as ItemsPresenter;
                 var VStackPanel = VisualTreeHelper.GetChild(ItemsPres as DependencyObject, 0) as VirtualizingStackPanel;
@@ -41,6 +52,60 @@ namespace Module.HeroVirtualTabletop.Roster
                     item.IsSelected = true;
                 }
                 e.Handled = true;
+            }
+        }
+        private void ListViewItem_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 1)
+            {
+                isSingleClick = true;
+
+                // Start the click timer.
+                clickTimer.Start();
+            }
+            // This is the second mouse click.
+            else if (e.ClickCount == 2)
+            {
+                isDoubleClick = true;
+            }
+            else if (e.ClickCount == 3)
+            {
+                isTripleClick = true;
+            }
+            else if (e.ClickCount == 4)
+            {
+                isQuadrupleClick = true;
+            }
+            
+        }
+
+        void clickTimer_Tick(object sender, EventArgs e)
+        {
+            milliseconds += 50;
+
+            if (milliseconds >= maxClickTime)
+            {
+                clickTimer.Stop();
+
+                if (isQuadrupleClick)
+                {
+                    this.viewModel.ToggleManueverWithCamera();
+                }
+                else if(isTripleClick)
+                {
+                    this.viewModel.TargetAndFollow();
+                }
+                else if (isDoubleClick)
+                {
+                    this.viewModel.TargetAndFollow();
+                }
+                else
+                {
+                    this.viewModel.TargetOrFollow();
+                }
+
+                isSingleClick = isDoubleClick = isTripleClick = isQuadrupleClick = false;
+                milliseconds = 0;
             }
         }
     }
