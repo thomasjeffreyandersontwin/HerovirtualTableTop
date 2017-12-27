126fda4fea52da383997ec284283b36316c2fb30
thomasjeffrey <thomasjeffreyandersontwin@gmail.com>
2017-12-27 15:55:47 -0500

    small change

--- HeroVirtualTabletop.WPF/Modules/Module.HeroVirtualTabletop/HCSIntegration/HCSIntegrator.cs
+++ HeroVirtualTabletop.WPF/Modules/Module.HeroVirtualTabletop/HCSIntegration/HCSIntegrator.cs
@@ -926,7 +926,7 @@ namespace Module.HeroVirtualTabletop.HCSIntegration
                     break;
                 case "jumping":
                 case "jump":
-                    movementName = "Jumping";
+                    movementName = "Leaping";
                     break;
             }
             return movementName;
